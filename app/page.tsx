12832c442c1387f67dc311e869f8dfa8d089dd9d
aniketch07 <caniket1307@gmail.com>
2025-01-22 23:53:23 +0530

    made UI changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,3 @@
-// 'use client'
-
-// import React from "react";
-// import Link from 'next/link';
-
-// export default function Page() {
-//   return (
-//     <div className="p-4">
-//       <h1 className="text-4xl font-bold text-center mb-4">Welcome to the Home Page</h1>
-//       <p className="text-lg text-center mb-4">This is the home page content.</p>
-
-//       <div className="text-center">
-//         <Link href="/signup" className="text-blue-500 hover:text-blue-700">Sign Up</Link> | 
-//         <Link href="/signin" className="text-blue-500 hover:text-blue-700">Sign In</Link> | 
-//         <Link href="/adddatapage" className="text-blue-500 hover:text-blue-700">Add Data</Link>|
-//         <Link href="/getdata" className="text-blue-500 hover:text-blue-700">Get Data</Link> {/* Add this link */}
-//       </div>
-//     </div>
-//   );
-// }
-
 'use client'
 
 import React from "react";
@@ -28,9 +7,18 @@ export default function Page() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col justify-center items-center">
       <h1 className="text-5xl font-extrabold mb-6 text-center">Welcome to the Home Page</h1>
-      <p className="text-xl text-gray-300 mb-8 text-center">Discover the power of simplicity with our features.</p>
-
+      <p className="text-xl text-gray-300 mb-8 text-center">Betr Beta task1----Aniket Chaudhary</p>
       <div className="flex flex-wrap justify-center gap-6">
+        <Link href="https://www.linkedin.com/in/aniket-chaudhary-a8b261264/" className="px-4 py-2 bg-gray-700 text-white rounded-lg shadow-lg hover:bg-gray-800 transition">
+          LinkedIn
+        </Link>
+        <Link href="https://github.com/aniketch07" className="px-4 py-2 bg-gray-700 text-white rounded-lg shadow-lg hover:bg-gray-800 transition">
+          GitHub
+        </Link>
+      </div><br />
+      <p className="text-xl text-gray-300 mb-8 text-center">Task : create full stack app with nextjs13 and firebase</p>
+
+      <div className="flex flex-wrap justify-center gap-6 mb-8">
         <Link href="/signup" className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition">
           Sign Up
         </Link>
@@ -44,6 +32,8 @@ export default function Page() {
           Get Data
         </Link>
       </div>
+
+      
     </div>
   );
 }
